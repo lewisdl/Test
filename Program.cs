9ba01c12d437e709a2a1b0d6b03e9e5b6a550828
Des-Marr Lewis <lewisdl@email.sc.edu>
2023-05-08 19:43:58 -0400

    Using old template

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MyFirstApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
